be8cbbb17f4ec53cdcbbcf97f31483ad2d8ea687
DiegoHernandez <diego0930@gmail.com>
2024-08-11 22:27:15 -0500

    Configurar la Comunicación entre Componentes

--- PortfolioDiegoHernandez/src/app/domains/main-content/main-content.component.ts
+++ PortfolioDiegoHernandez/src/app/domains/main-content/main-content.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { ProyectosComponent } from '../proyectos/proyectos.component';
+import { HabilidadesComponent } from '../habilidades/habilidades.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [ProyectosComponent,HabilidadesComponent,ContactoComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
 export class MainContentComponent {
-
+  @Input() currentSection: string = 'proyectos';
 }
